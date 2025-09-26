ebea656f9125e9e768d6607b1390fb193a0a19ce
Nova Vina <20081125@mail.wit.ie>
2025-09-26 12:20:13 +0100

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -9,3 +9,38 @@ function allOlder(friends: Friend[]): string[] {
   }
 
 console.log(allOlder(friends))
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+    const result = cs.sort(
+      (c1, c2) => c1.contact.extension - c2.contact.extension
+    );
+    return result[cs.length - 1];
+  }
+  console.log(highestExtension(colleagues.current));
+  
+  // Add a colleague to the array with extension = highest + 1
+function addColleague(
+    cs: Colleague[],
+    name: string,
+    department: string,
+    email: string
+  ): void {
+    const highest = highestExtension(cs).contact.extension;
+  
+    const newColleague: Colleague = {
+      name,
+      department,
+      contact: {
+        email,
+        extension: highest + 1,
+      },
+    };
+  
+    cs.push(newColleague);
+  }
+  
+  addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+    
+  console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+
